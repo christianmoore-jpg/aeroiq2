50dfe3757917d0871d741f1d753681d9213a0ae0
christianmoore-jpg <christian.mooreurban@gmail.com>
2026-04-10 11:16:52 -0400

    Add files via upload

---
+++ files/sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'aeroiq-v1';
+const STATIC = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  // Network-first for API calls, cache-first for static assets
+  if (url.hostname !== location.hostname) {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(cached => {
+        const net = fetch(e.request).then(res => {
+          if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+          return res;
+        });
+        return cached || net;
+      })
+    );
+  }
+});
